a30496c0f9aa78a73e616090c51c8ce7ab16fd1c
vishnuvardhanalugandula@gmail.com <kaithaprakash2107@gmail.com>
2025-11-05 15:16:16 -0800

    intial

---
+++ swap.java
@@ -0,0 +1,11 @@
+class swap{
+public static void main(String[] args){
+int a=10,b=20;
+int temp=0;
+System.out.println("before a="+a+ "b="+b);
+temp=a;
+a=b;
+b=temp;
+System.out.println("after swaping a"+a+ "b="+b);
+}
+}
